5fe0b8ad05373e2dce89779e2b98ca9e1194d877
kaz0636 <kaz0636@gmail.com>
2025-08-12 12:41:42 +0900

    Updates

---
+++ main.d.ts
@@ -0,0 +1,6 @@
+import "./styles.scss";
+import type { JSNViewOptions } from "./types";
+declare global {
+    function jsnview(data: any, options?: JSNViewOptions): HTMLElement;
+}
+//# sourceMappingURL=main.d.ts.map
